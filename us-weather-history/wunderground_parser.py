7d18433f1fb225c43e931617090b00046086bee1
Nicholas Young <nicholastesauroyoung@gmail.com>
2016-03-08 23:27:05 -0800

    committing final changes to wunderground_parser.py and KSAF.csv

--- us-weather-history/wunderground_parser.py
+++ us-weather-history/wunderground_parser.py
@@ -33,49 +33,78 @@ p
                                                 current_date.day)) as in_file:
                 soup = BeautifulSoup(in_file.read(), 'html.parser')
 
-                weather_data = soup.find(id='historyTable').find_all('span', class_='wx-value')
+                weather_data_rows = soup.find(id='historyTable').find_all('tr')
+                weather_data = []
+                for i in range(len(weather_data_rows)):
+                    soup1 = weather_data_rows[i]
+                    weather_data.append(soup1.find_all('span', class_='wx-value'))
+                    weather_data = [x for x in weather_data if x != []]
+                
+                if len(weather_data[4]) < 2:
+                    weather_data[4].append(None)
+                    weather_data[4].append(None)
+
+
                 weather_data_units = soup.find(id='historyTable').find_all('td')
 
+
                 try:
-                    actual_mean_temp = weather_data[0].text
-                    actual_max_temp = weather_data[2].text
-                    average_max_temp = weather_data[3].text
-                    record_max_temp = weather_data[4].text
-                    actual_min_temp = weather_data[5].text
-                    average_min_temp = weather_data[6].text
-                    record_min_temp = weather_data[7].text
+                    actual_mean_temp = weather_data[0][0].text
+                    actual_max_temp = weather_data[1][0].text
+                    average_max_temp = weather_data[1][1].text
+                    if weather_data[1][2]:
+                        record_max_temp = weather_data[1][2].text
+                    actual_min_temp = weather_data[2][0].text
+                    average_min_temp = weather_data[2][1].text
+                    record_min_temp = weather_data[2][2].text
                     record_max_temp_year = weather_data_units[
                         9].text.split('(')[-1].strip(')')
                     record_min_temp_year = weather_data_units[
                         13].text.split('(')[-1].strip(')')
 
-                    actual_precipitation = weather_data[9].text
+                    actual_precipitation = weather_data[4][0].text
                     if actual_precipitation == 'T':
-                        actual_precipitation = '0.0'
-                    average_precipitation = weather_data[10].text
-                    record_precipitation = weather_data[11].text
+                        actual_precipitation = '0.0' 
+
+                    #Test whether station collets average or record precipitation data
+                    if weather_data[4][1]:
+                        average_precipitation = weather_data[4][1].text
+                    else: average_precipitation = None
+                    if weather_data[4][2]:
+                        record_precipitation = weather_data[4][2].text
+                    else: record_precipitation = None
 
                     # Verify that the parsed data is valid
                     if (record_max_temp_year == '-1' or record_min_temp_year == '-1' or
                             int(record_max_temp) < max(int(actual_max_temp), int(average_max_temp)) or
                             int(record_min_temp) > min(int(actual_min_temp), int(average_min_temp)) or
-                            float(actual_precipitation) > float(record_precipitation) or
-                            float(average_precipitation) > float(record_precipitation)):
-                        raise Exception
+                            ((record_precipitation is not None or average_precipitation is not None) and (float(actual_precipitation) > float(record_precipitation) or
+                            float(average_precipitation) > float(record_precipitation)))):
+                        #raise Exception
+                        print("exception")
 
                     out_file.write('{}-{}-{},'.format(current_date.year, current_date.month, current_date.day))
                     out_file.write(','.join([actual_mean_temp, actual_min_temp, actual_max_temp,
                                              average_min_temp, average_max_temp,
                                              record_min_temp, record_max_temp,
                                              record_min_temp_year, record_max_temp_year,
-                                             actual_precipitation, average_precipitation,
-                                             record_precipitation]))
+                                             actual_precipitation,]))
+                    if average_precipitation:
+                        out_file.write(',{}'.format(average_precipitation))
+                    if record_precipitation:
+                        out_file.write(',{}'.format(record_precipitation))
+
                     out_file.write('\n')
                     current_date += timedelta(days=1)
+                    
                 except:
                     # If the web page is formatted improperly, signal that the page may need
                     # to be downloaded again.
                     try_again = True
+                    
+                    '''#Used to produce a mock-up .csv for KSAF
+                    print("skipped", current_date)
+                    current_date += timedelta(days=1)'''
 
             # If the web page needs to be downloaded again, re-download it from
             # wunderground.com
@@ -105,5 +134,5 @@ p
 
 # Parse the stations used in this article
 for station in ['KCLT', 'KCQT', 'KHOU', 'KIND', 'KJAX',
-                'KMDW', 'KNYC', 'KPHL', 'KPHX', 'KSEA']:
+                'KMDW', 'KNYC', 'KPHL', 'KPHX', 'KSEA', 'KSAF']:
     parse_station(station)
